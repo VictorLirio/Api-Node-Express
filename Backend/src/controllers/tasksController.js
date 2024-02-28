89d9312ea2d1867dbd775eecc2e3dd85aa7805f2
Victor Domingos <victordias_jobs@hotmail.com>
2024-02-28 20:37:22 -0300

    feat: :sparkles: requests working

--- Backend/src/controllers/tasksController.js
+++ Backend/src/controllers/tasksController.js
@@ -1,11 +1,38 @@
-const tasksModels = require('../models/tasksModels');
+const tasksModel = require('../models/tasksModel');
 
 const getAll = async (req, res) => {
-    const tasks = await tasksModels.getAll();
+    const tasks = await tasksModel.getAll();
     return res.status(200).json(tasks);
 };
     
+const createTask = async (request, response) => {
+   const createdTask = await tasksModel.createTask(request.body);
+    return response.status(201).json(createdTask);
+  };
+
+  const deleteTask = async (request, response) => {
+
+      const { id } = request.params;
+
+      await tasksModel.deleteTask(id);
+
+      return response.status(204).json({message: 'Task deleted'});
+  }
+
+
+  const updateTask = async (request, response) => {
+
+    const { id } = request.params
+
+    await tasksModel.updateTask(id, request.message);
+
+    return response.status(204).json({message: 'Task updated'});
+
+  }
 
 module.exports = {
-    getAll
+    getAll,
+    createTask,
+    deleteTask,
+    updateTask,
 };
---
+++ Backend/src/models/tasksModel.js
@@ -0,0 +1,40 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+    const tasks = await connection.execute('SELECT * FROM tasks');
+    return tasks[0]
+};
+
+const createTask = async (task) => {
+    const { title } = task;
+    const dateUTC = new Date(Date.now()).toUTCString();
+  
+    const query = 'INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)';
+  
+    const [createdTask] = await connection.execute(query, [task.title, 'pendente', dateUTC]);
+    return {createdTask};
+  };
+
+  const deleteTask = async (id) => {
+
+    const query = 'DELETE FROM tasks WHERE id =?';
+    const [deletedTask] = await connection.execute(query, [id]);
+    return {deletedTask};
+
+  }
+
+  const updateTask = async (id, task) => {
+
+    const query = 'UPDATE tasks SET title = ?, status = ?, WHERE id =?'; 
+
+    const [updatedTask] = await connection.execute(query, [task.title, task.status, id]);
+    return {updatedTask};
+
+  }
+
+module.exports = {
+    getAll,
+    createTask,
+    deleteTask,
+    updateTask,
+};
